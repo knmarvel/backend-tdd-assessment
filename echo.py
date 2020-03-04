69c312d8f65658233b614784936879282b30905e
Karen Marvel <knmarvel@gmail.com>
2020-03-04 11:54:29 -0500

    tests functioning and returning true

--- echo.py
+++ echo.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """An enhanced version of the 'echo' cmd line utility"""
 
-__author__ = "???"
+__author__ = "knmarvel"
 
 
 import sys
@@ -10,17 +10,49 @@ import argparse
 
 
 def create_parser():
-    parser = argparse.ArgumentParser(description = "Cause argparse help to run")
+    parser = argparse.ArgumentParser(
+        description="Perform transformation on input text.")
+    parser.add_argument("text",
+                        type=str,
+                        help="text to be manipulated")
+    parser.add_argument('-u',
+                        '--upper',
+                        action='store_const',
+                        const=bool,
+                        help="convert text to uppercase")
+    parser.add_argument("-l",
+                        "--lower",
+                        action='store_const',
+                        const=bool,
+                        help="convert text to lowercase")
+    parser.add_argument("-t",
+                        "--title",
+                        action='store_const',
+                        const=bool,
+                        help="convert text to titlecase")
     return parser.parse_args()
 
 
-def main(args):
+def main():
     """Implementation of echo"""
-    args = create_parser()
-    print(args)
 
-    pass
+    args = create_parser()
+
+    if args.upper:
+        if sys.argv[1] == "-u" or sys.argv[1] == "--upper":
+            print(args.text.upper())
+
+    if args.lower:
+        if sys.argv[1] == "-l" or sys.argv[1] == "--lower":
+            print(args.text.lower())
+
+    if args.title:
+        if sys.argv[1] == "-t" or sys.argv[1] == "--title":
+            print(args.text.title())
+
+    else:
+        print(args.text)
 
 
 if __name__ == '__main__':
-    pass
+    main()
--- tests/test_echo.py
+++ tests/test_echo.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-import echo
 import subprocess
 
 
@@ -12,38 +11,118 @@ class TestEcho(unittest.TestCase):
         """ Running the program without arguments should show usage. """
 
         # Run the command `python ./echo.py -h` in a separate process, then
-        # collect it's output.
+        # collect its output.
         process = subprocess.Popen(
             ["python", "./echo.py", "-h"],
             stdout=subprocess.PIPE)
         stdout, _ = process.communicate()
+        stdout = stdout.decode("utf-8")
         usage = open("./USAGE", "r").read()
-
         self.assertEquals(stdout, usage)
 
     def test_upper(self):
-        """Running the program with -u or --upper as arguments should result with "upper" stored in namespace returned from
-         parser.parse_args and that "hello" will return to "HELLO" when the program is run"""
+        """Running the program with -u or --upper as arguments should result with
+        "upper" stored in namespace returned from parser.parse_args and that
+        "hello" will return to "HELLO" when the program is run"""
+
+        # Run the command `python ./echo.py -u hello` in a separate process,
+        # then collect its output.
+        process = subprocess.Popen(
+            ["python", "./echo.py", "-u", "hello"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        stdout_u = stdout.decode("utf-8")
+        process = subprocess.Popen(
+            ["python", "./echo.py", "--upper", "hello"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        stdout_upper = stdout.decode("utf-8")
+        usage = open("./USAGEUPPER", "r").read()
+        self.assertEquals(stdout_u, usage)
+        self.assertEquals(stdout_upper, usage)
 
     def test_lower(self):
         """Running the program with -l or --lower as arguments should result with
         "lower" stored in namespace returned from parser.parse_args and that
         "Hello" will return to "hello" when the program is run"""
 
+        process = subprocess.Popen(
+            ["python", "./echo.py", "-l", "Hello"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        stdout_l = stdout.decode("utf-8")
+        process = subprocess.Popen(
+            ["python", "./echo.py", "--lower", "Hello"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        stdout_lower = stdout.decode("utf-8")
+        usage = open("./USAGELOWER", "r").read()
+        self.assertEquals(stdout_l, usage)
+        self.assertEquals(stdout_lower, usage)
+
     def test_title(self):
         """Running the program with -t or --title as arguments should result with
         "title" stored in namespace returned from parser.parse_args and that
         "hello" will return to "Hello" when the program is run"""
-    
+
+        process = subprocess.Popen(
+            ["python", "./echo.py", "-t", "hello"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        stdout_t = stdout.decode("utf-8")
+
+        process = subprocess.Popen(
+            ["python", "./echo.py", "--title", "hello"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        stdout_title = stdout.decode("utf-8")
+
+        usage = open("./USAGETITLE", "r").read()
+
+        self.assertEquals(stdout_t, usage)
+        self.assertEquals(stdout_title, usage)
+
     def test_all(self):
         """Running the program with all three arguments should result with
         the first argument stored in namespace returned from
         parser.parse_args and that the first argument's case will be applied
         to the returned string"""
-    
+
+        process = subprocess.Popen(
+            ["python", "./echo.py", "-t", "-u", "-l", "hello"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        stdout_t = stdout.decode("utf-8")
+        usage_t = open("./USAGETITLE", "r").read()
+        self.assertEquals(stdout_t, usage_t)
+
+        process = subprocess.Popen(
+            ["python", "./echo.py", "-u", "-t", "-l", "hello"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        stdout_u = stdout.decode("utf-8")
+        usage_u = open("./USAGEUPPER", "r").read()
+        self.assertEquals(stdout_u, usage_u)
+
+        process = subprocess.Popen(
+            ["python", "./echo.py", "-l", "-u", "-t", "hello"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        stdout_l = stdout.decode("utf-8")
+        usage_l = open("./USAGELOWER", "r").read()
+        self.assertEquals(stdout_l, usage_l)
+
     def test_none(self):
         """Running the program with no arguments should result with
         the string being returned as-is"""
+        process = subprocess.Popen(
+            ["python", "./echo.py", "hElLo"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        stdout = stdout.decode("utf-8")
+        usage = open("./USAGENONE", "r").read()
+        self.assertEquals(stdout, usage)
+
 
 if __name__ == '__main__':
     unittest.main()
